16f2df969cc82f20e103ba6321f6abdd1cff2d77
MilanoGamezzYT <99067157@mydavinci.nl>
2023-04-20 11:34:22 +0200

    AVI score 20-04-2023

---
+++ AVI/program.py
@@ -0,0 +1,34 @@
+from functies import *
+import tkinter as tk
+from tkinter import ttk
+import tkinter.messagebox # if you want to send messages to the user.
+
+# functions
+def calculate():
+    textToBeCalculated = calculateInput.get('1.0', 'end') # get all the text from inputfield
+    characterLabel.config(text=f"Karakters: {getNumberOfCharacters(textToBeCalculated)}")
+    sentencesLabel.config(text =f"Zinnen: {getNumberOfSentences(textToBeCalculated)}")
+    WordsLabel.config(text =f"Woorden: {getNumberOfWords(textToBeCalculated)}")
+    AVIScoreLabel.config(text =f"AVI: {getAverageAVI(textToBeCalculated)}")
+
+
+#variables TK
+root = tk.Tk()              # create tkInter window
+root.title('Text analyser') # set title
+root.geometry('600x600')    # set dimension
+calculateInput = tk.Text(root, width = 70, height = 30, background='lightgrey')            # generate imput element
+calculateButton = ttk.Button(root, text='Bereken score(s)', command=calculate)        # generate button when pressed -> calculate
+characterLabel = tk.Label(root, text=f'Karakters:', width=20, bg='black', fg='white') # generate characterLabel
+sentencesLabel = tk.Label(root, text=f'Zinnen:', width=20, bg='black', fg='white')   # generate sentencesLabel
+WordsLabel = tk.Label(root, text=f'Woorden:', width=20, bg='black', fg='white')
+AVIScoreLabel = tk.Label(root, text=f'AVI Score:', width=20, bg='black', fg='white')  # generate AVIScoreLabel
+
+calculateInput.place(x=20, y=20)   # place is one of the ways to put elements on root (window).
+calculateButton.place(x=20, y=520)
+characterLabel.place(x=180, y=560)
+sentencesLabel.place(x=20, y=560)
+WordsLabel.place(x=180, y=530)
+AVIScoreLabel.place(x=340, y=560)
+
+# start program
+root.mainloop() # runs until stopped with default stop button.
